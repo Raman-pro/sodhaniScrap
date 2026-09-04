5720e9ce38f8a75e2962d4d4a0a0512b6bf39ea5
anshmangla <anshmangla3107@gmail.com>
2026-09-04 18:19:32 +0530

    debug: check all L&T ticks

--- src/scripts/check_prev_close.ts
+++ src/scripts/check_prev_close.ts
@@ -6,7 +6,7 @@ async function run() {
     const res = await client.query(`
       SELECT hp2.record_date, hp2.close_price, hp2.adj_close
       FROM historical_prices hp2
-      WHERE hp2."FinInstrmId" = '500510' AND DATE(hp2.record_date) = '2026-09-03'
+      WHERE hp2."FinInstrmId" = '500510' 
       ORDER BY 
           hp2.record_date DESC
       LIMIT 10
